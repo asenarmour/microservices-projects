f62bd84a882c90d8c8d038083fbcfc2adf9c78e8
anurag negi <anuragnegi008@gmail.com>
2021-03-16 12:38:26 +0530

    first commit

---
+++ project1/property-access-service/src/main/java/com/anurag/microservices/propertyaccessservice/controller/PropertyFileAccessController.java
@@ -0,0 +1,23 @@
+package com.anurag.microservices.propertyaccessservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anurag.microservices.propertyaccessservice.beans.PropertyAccessBean;
+import com.anurag.microservices.propertyaccessservice.beans.PropertyAccessValue;
+
+@RestController
+@RequestMapping("/access")
+public class PropertyFileAccessController {
+
+	@Autowired
+	PropertyAccessBean propertyAccessBean;
+	
+	@GetMapping("/accessPropertyFile")
+	public PropertyAccessValue accessPropertyFile() {
+		return new PropertyAccessValue(propertyAccessBean.getName(),
+				propertyAccessBean.getDescription());
+	}
+}
